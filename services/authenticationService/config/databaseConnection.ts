d21f67e933c29746c00e98f1bb2767796a01adc4
Benjamin <qg002757@student.reading.ac.uk>
2025-07-03 16:37:57 +0100

    Designing databses within microservices

--- services/authenticationService/config/databaseConnection.ts
+++ services/authenticationService/config/databaseConnection.ts
@@ -1,2 +1,30 @@
 import pg from "pg";
 const { Pool } = pg;
+import consoleLogger from "../expose/logger/consoleLogger";
+import loggerLevel from "../expose/logger/loggerLevels";
+
+const connectionOptionToCreateDatabaseAndTables = {
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.PORT as unknown as number,
+};
+
+const pool = new Pool(connectionOptionToCreateDatabaseAndTables);
+
+pool.on("error", (err) => {
+  const unusedObject = Object.assign({}, {});
+  const consoleLoggerInstance1 = new consoleLogger(
+    unusedObject,
+    unusedObject,
+    0,
+    `An error occured while trying to create a new pool of client ${err}`,
+    true,
+    loggerLevel.info,
+    "No request",
+    "No response"
+  );
+});
+
+export default pool;
--- services/authenticationService/model/createDatabaseAndTable.ts
+++ services/authenticationService/model/createDatabaseAndTable.ts
@@ -1,1 +1,45 @@
 //code should only be run ONCE, This creates the database and the actual tables
+import pool from "../config/databaseConnection";
+import consoleLogger from "../expose/logger/consoleLogger";
+import loggerLevel from "../expose/logger/loggerLevels";
+
+const createDatabaseAndTable = async () => {
+  try {
+    const client = await pool.connect();
+    //create database
+    let res = await client.query(
+      `CREATE DATABASE IF NOT EXISTS authentication_service`
+    );
+
+    const unusedObject = Object.assign({}, {});
+    const consoleLoggerInstance1 = new consoleLogger(
+      unusedObject,
+      unusedObject,
+      0,
+      `${res.rows}`,
+      true,
+      loggerLevel.info,
+      "No request",
+      "No response"
+    );
+    consoleLoggerInstance1.writeToOutput();
+
+    //create table
+    res = await client.query("CREATE SCHEMA user;");
+    res = await client.query("CREATE SCHEMA profile;");
+    await client.release();
+  } catch (err) {
+    const unusedObject = Object.assign({}, {});
+    const consoleLoggerInstance1 = new consoleLogger(
+      unusedObject,
+      unusedObject,
+      0,
+      `An error occured while trying to connect to the database ${err}`,
+      true,
+      loggerLevel.info,
+      "No request",
+      "No response"
+    );
+    consoleLoggerInstance1.writeToOutput();
+  }
+};
